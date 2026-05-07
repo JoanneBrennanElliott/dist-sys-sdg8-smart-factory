9c08d1eeb219cbd7ce9ce760a86112b886ff9485
JoanneBrennanElliott <x23410001@student.ncirl.ie>
2026-05-07 19:58:26 +0100

    2 updates
    removed console.log 
     can changed     =  __dirname,
      "../server/protos/temperature.proto"

--- server-side/TemperatureSafetyService/GUI/bridge.js
+++ server-side/TemperatureSafetyService/GUI/bridge.js
@@ -10,11 +10,32 @@ app.use(cors());
 app.use(express.json());
 
 // Load proto
-const PROTO_PATH = __dirname + "/protos/temperature.proto";
-const packageDef = protoLoader.loadSync(PROTO_PATH, {});
+//const PROTO_PATH = __dirname + "/protos/temperature.proto";
+//const packageDef = protoLoader.loadSync(PROTO_PATH, {});
+
+const path = require("path");
+const PROTO_PATH = path.join(
+  __dirname,
+  "../server/protos/temperature.proto"
+);
+//const PROTO_PATH = "C:/DSCA/ServerSide/TemperatureSafetyService/server/protos/temperature.proto";
+
+console.log("DIR:", __dirname);
+console.log("PROTO_PATH:", PROTO_PATH);
+
+const packageDef = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
+});
 const grpcObj = grpc.loadPackageDefinition(packageDef);
+//console.log("Loaded gRPC object:", grpcObj);    // for testing purposes 
+
 const client = new grpcObj.temperature.TemperatureSafetyService(
-  "localhost:50051",
+//  "localhost:50051",
+   "localhost:40000", // changed bridge to connect to 40000 to get gui working
   grpc.credentials.createInsecure()
 );
 
@@ -31,15 +52,41 @@ app.post("/MonitorTemperatureStatus", (req, res) => {
     threshold: Number(threshold)
   };
   
-	client.MonitorTemperatureStatus( request , (err, response) => {
-		if (err) {
-		console.error("gRPC error:", err);
-		return res.status(500).json({ error: "gRPC request failed" });
-      //return res.status(500).json({ error: err.message });
-    }
-    res.json(response);
+//  console.log("Available gRPC methods:", client);     // for testing purposes 
+//	console.log("Client methods:", Object.keys(Object.getPrototypeOf(client)));
+
+//	client.MonitorTemperatureStatus( request , (err, response) => {
+//		if (err) {
+//		console.error("gRPC error:", err);
+//		return res.status(500).json({ error: "gRPC request failed" });
+  //    //return res.status(500).json({ error: err.message });
+//    }
+
+//	const call = client.MonitorTemperatureStatus(request);
+// added required metadata
+	const metadata = new grpc.Metadata();
+	metadata.add("authorization", "Bearer TOKEN_123");
+	metadata.add("trace-id", "web-ui-123");
+	metadata.add("client-id", "browser");
+
+	const call = client.MonitorTemperatureStatus(request, metadata);
+
+	let results = [];
+
+	call.on("data", (response) => {
+		console.log("Temperature update:", response);
+		results.push(response);
+	});
+
+	call.on("end", () => {
+		console.log("Stream ended");
+		res.json({ stream: results });
+	});
+
+	call.on("error", (err) => {
+		console.error("Stream error:", err);   
+		res.status(500).json({ error: "gRPC streaming failed" });});
   });
-});
 
 // Start Express
 app.listen(3000, () => {
