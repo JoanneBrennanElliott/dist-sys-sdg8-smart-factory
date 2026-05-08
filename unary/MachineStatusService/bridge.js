a156b415e0053c6020377773945c1e925edcc0ea
JoanneBrennanElliott <x23410001@student.ncirl.ie>
2026-05-08 13:54:01 +0100

    added metadata
    app.use(express.json());
    
    app.use(cors());

--- unary/MachineStatusService/bridge.js
+++ unary/MachineStatusService/bridge.js
@@ -1,11 +1,15 @@
 //bridge to connect GUI to gRPC
 
 const express = require("express");
+const cors = require("cors");     // need this cor package 
 const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
+
 
 // Load proto
 const PROTO_PATH = __dirname + "/protos/unary.proto";
@@ -16,11 +20,21 @@ const client = new grpcObj.unary.MachineStatusService(
   grpc.credentials.createInsecure()
 );
 
+//console.log("DIR:", __dirname);			//testing
+//console.log("PROTO_PATH:", PROTO_PATH);
+
 // HTTP endpoint the GUI will call
 app.post("/checkMachineStatus", (req, res) => {
   const machineId = req.body.machineId;
+  
+  	 // added metadata
+	const metadata = new grpc.Metadata();
+	metadata.add("machine-id", "machine-01");
+	metadata.add("trace-id", "web-ui-123");
+	metadata.add("authorization", "Bearer TOKEN_123");
+	
 
-  client.CheckMachineStatus({ machineId }, (err, response) => {
+  client.CheckMachineStatus({ machineId },metadata, (err, response) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
